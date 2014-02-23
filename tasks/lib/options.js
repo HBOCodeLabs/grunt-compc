ed777b32fb2b55d531d559b315def4a57a0addd7
Artem Abashev <artem@abashev.com>
2014-02-23 17:58:33 +0400

    Compc options reader.

---
+++ tasks/lib/options.js
@@ -0,0 +1,17 @@
+/*
+ * grunt-compc
+ * https://github.com/artema/grunt-compc
+ *
+ * Copyright (c) 2014 Artem Abashev
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = {
+	getDefaultOptions: function() {
+		return {
+			
+		};
+	}
+};
