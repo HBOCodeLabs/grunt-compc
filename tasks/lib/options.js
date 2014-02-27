6883c53af8a13abb380f835a90317a5328f2e862
Artem Abashev <artem@abashev.com>
2014-02-28 01:46:41 +0400

    Compc options.

--- tasks/lib/options.js
+++ tasks/lib/options.js
@@ -11,11 +11,45 @@
 module.exports = {
 	getDefaultOptions: function () {
 		return {
-			
+			'output': null,
+			'compute-digest': false,
+			'directory': false,
+			'include-lookup-only': false,
+			'include-classes': [],
+			'include-file': [],
+			'include-namespaces': [],
+			'include-sources': [],
+			'include-stylesheet': []
 		};
 	},
 	
 	toCommandLine: function (options) {
-		return options.toString();
+		return Object.keys(options).map(function(key) {
+			var value = options[key];
+
+			if (key && value != null) {
+				switch (typeof value) {
+					case 'boolean':
+						if (value)
+							return '-' + key + '=' + value;
+						break;
+					case 'number':
+					case 'string':
+						return '-' + key + '=' + value;
+
+					default:
+						if (value instanceof Array && value.length > 1) {
+							return key + ' ' + value.reduce(function (res, val) {
+								return '-' + res + ' ' + val;
+							});
+						}
+						break;
+			    }
+			}
+
+			return undefined;
+		})
+		.filter(Boolean)
+		.join(' ');
 	}
 };
