9d19056f316942a0452cc8dfd6ed7721f43bba61
David Wolfe <david.p.wolfe@gmail.com>
2015-05-05 10:47:38 -0700

    moving a line to prep for a de-dupe of code

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,8 +60,8 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
